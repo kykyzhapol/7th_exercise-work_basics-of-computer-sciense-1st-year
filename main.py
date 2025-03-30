f6f04dc3a52f8f61c76e41304c58525f9ab3431a
Sergey Chirkov <s.chirkov1@g.nsu.ru>
2025-03-31 01:30:21 +0700

    all is ready

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-'''
 #1st
 num = []
 dels = 0
@@ -18,6 +17,7 @@ for i in range(0, len(strr)):
         strn.append(strr[i])
 print(strn)
 
+#3rd
 import math as m
 q = 3
 while m.sqrt(q) - round(m.sqrt(q)) != 0:
@@ -26,15 +26,57 @@ while m.sqrt(q) - round(m.sqrt(q)) != 0:
         print('full sqrt')
         break
 
+#4th
 num = int(input('enter integer number -->'))
 print(sum(range(1, num+1)))
 
+#5th
 from numpy.ma.core import floor
 
 val_in = int(input())
 val_max = int(floor(val_in ** (1/3)))
 for i in range(1, val_max+1):
     print(i**3, end=' ')
-'''
 
+#6th
+subscribes = int(input())
+k = 1
+while subscribes > k:
+    print(k)
+    k*=2
 
+#7th
+answer = int(input())
+k = 1
+while answer > k:
+    k *= 2
+if k == answer:
+    print('верно')
+else:
+    print('неверно')
+
+#8th
+xmmm = int(input())
+k = 1
+while 2**k <= xmmm:
+    k += 1
+print(k)
+
+#9th
+lst = list(map(int, input().split()))
+prot = 1 + (lst[1]/100)
+days = 1
+while lst[2] > lst[0]:
+    lst[0] *= prot
+    days +=1
+print(days)
+
+#10th
+temp = []
+lower = 0
+while int(input()) != 0:
+    temp.append(float(input()))
+for i in range(1, len(temp)):
+    if temp[i] > temp[i+1]:
+        lower += 1
+print(lower)
